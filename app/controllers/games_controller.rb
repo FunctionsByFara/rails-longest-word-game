75cb7c1410cc01b08fb1dc435750d89889221c2b
Fara Bashorun <farabashorun@gmail.com>
2023-02-20 15:59:51 +0000

    done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'open-uri'
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times { @letters << ('A'..'Z').to_a.sample }
+    @letters.shuffle!
+  end
+
+  def score
+    @letters = params[:letters].chars
+    @answer = params[:game].upcase!
+    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    response = URI.open(url).read
+    data = JSON.parse(response)
+    @english_word = data['found']
+    @original_letters = @letters
+    @answer = @answer.chars
+    @length = @letters.length - @answer.length
+    @answer.each { |letter| @letters.delete("#{letter}") if @letters.include? "#{letter}" }
+    @included if @letters.length == @length
+  end
+
+
+end
